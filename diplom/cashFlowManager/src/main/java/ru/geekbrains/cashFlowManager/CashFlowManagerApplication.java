d781a94f1a5b255a9b8ee5dc5cac690cc6d2a0c0
Syslop <petrovkonstantined@gmail.com>
2024-02-05 03:36:11 +0300

    init project

---
+++ diplom/cashFlowManager/src/main/java/ru/geekbrains/cashFlowManager/CashFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.cashFlowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowManagerApplication.class, args);
+	}
+
+}
---
+++ diplom/cashFlowManager/src/test/java/ru/geekbrains/cashFlowManager/CashFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.cashFlowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
