2a0859b0daba168f9d915c9833a43e26c0febd4e
Syslop <petrovkonstantined@gmail.com>
2024-02-13 22:50:16 +0300

    Убраны лишние зависимости, добавлено отображение спецификации по адресу http://localhost:8080/swagger-ui/index.html#/

--- diplom/cashFlowManager/src/test/java/ru/geekbrains/cashFlowManager/CashFlowManagerApplicationTests.java
+++ diplom/cashFlowManager/src/test/java/ru/geekbrains/cashFlowManager/CashFlowManagerApplicationTests.java
@@ -2,12 +2,12 @@ package ru.geekbrains.cashFlowManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootTest
 class CashFlowManagerApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
